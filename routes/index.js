a3240525e4cf4bad16c9325f7d2a873d50c4b50c
soFly <1351370237@qq.com>
2019-01-10 23:15:28 +0800

    V0.15

--- routes/index.js
+++ routes/index.js
@@ -5,53 +5,55 @@ var db = require('../db/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MkStone' });
+    res.render('index', { title: 'MkStone' });
 });
 
 
 router.post('/api/other/wx/tb/midpage/', function(req, res, next) {
-  var reqBody = req.body
-  var twp = reqBody.twp
-  console.log('twp', twp)
-  
-  var api_url = `http://api.taokouling.com/tkl/tkljm?apikey=yyEARRKhpk&tkl=${twp}`;
-  fetch(api_url, {})
-    .then((response) => {
-      if (response.ok) {
-        response.json().then(data => {
-          console.log(data);
-          var picUrl = data.picUrl || data.msg;
-          var route = Date.now().toString();
-          db.add(reqBody.twp, route, reqBody.alipay, reqBody.more, reqBody.xiaoweiba, picUrl, function() {
-            res.send({
-              route: `http://13.115.66.202:3000/twp/${route}`,
-              picUrl: picUrl,
-            });
-          })
-        })
-      } 
-      // 可以透過 blob(), json(), text() 轉成可用的資訊
+    var reqBody = req.body
+    var twp = reqBody.twp
+    var ip = '193.112.75.80'
+    console.log('请求体:', reqBody)
+    console.log('淘口令', twp)
 
-    }).catch((err) => {
-      console.log('錯誤:', err);
-    });
+    var api_url = `http://api.taokouling.com/tkl/tkljm?apikey=yyEARRKhpk&tkl=${twp}`;
+    fetch(api_url, {})
+        .then((response) => {
+            if (response.ok) {
+                response.json().then(data => {
+                    console.log(data);
+                    var picUrl = data.picUrl || data.msg;
+                    var route = Date.now().toString();
+                    db.add(reqBody.twp, route, reqBody.alipay, reqBody.more, reqBody.xiaoweiba, picUrl, function() {
+                        res.send({
+                            route: `http://${ip}:3000/twp/${route}`,
+                            picUrl: picUrl,
+                        });
+                    })
+                })
+            }
+            // 可以透過 blob(), json(), text() 轉成可用的資訊
+
+        }).catch((err) => {
+            console.log('錯誤:', err);
+        });
 });
 
 
 
-router.get('/twp/*', function(req, res, next) { 
-  var route = req.url.replace(/\/twp\//g, '')
-  db.findNoteByRoute(route, function(err, row) {
-    var picUrl = row.picUrl
-    res.render('taokouling', {
-      koulingPre: '复制框内整段文字，打开「手机掏寶ApP」即可「领取秘密券」并购买',
-      kouling: row.twp,
-      alipay: row.alipay,
-      wangzhan: row.more,
-      xiaoweiba: row.xiaoweiba,
-      picUrl: row.picUrl,
+router.get('/twp/*', function(req, res, next) {
+    var route = req.url.replace(/\/twp\//g, '')
+    db.findNoteByRoute(route, function(err, row) {
+        var picUrl = row.picUrl
+        res.render('taokouling', {
+            koulingPre: '复制框内整段文字，打开「手机掏寶ApP」即可「领取秘密券」并购买',
+            kouling: row.twp,
+            alipay: row.alipay,
+            wangzhan: row.more,
+            xiaoweiba: row.xiaoweiba,
+            picUrl: row.picUrl,
         })
-      })
+    })
 });
 
 module.exports = router;

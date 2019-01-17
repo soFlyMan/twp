d9b94f10e10673994feee480203de3b042774028
soFly <1351370237@qq.com>
2019-01-17 12:21:52 +0800

    V0.1

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var upload = require('jquery-file-upload-middleware');
 
 var db = require('./db/db');
 var index = require('./routes/index');
@@ -12,11 +13,19 @@ var users = require('./routes/users');
 var app = express();
 
 db.connect(function() {
-  console.log('数据库连接！')
+    console.log('数据库连接！')
 });
 
+upload.configure({
+    uploadDir: __dirname + '/public/uploads',
+    uploadUrl: '/text',
+    acceptFileTypes: /.+.txt/i,
+    maxPostSize: 30000000,
+    minFileSize: 1,
+    maxFileSize: 30000000,
+});
 // db.setup(function() {
-  // console.log('创建淘口令数据库')
+// console.log('创建淘口令数据库')
 // })
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,23 +41,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/text', upload.fileHandler());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch');
-var db = require('../db/db');
+// var db = require('../db/db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index.html', { title: 'MkStone' });
+// router.get('/', function(req, res, next) {
+//     res.render('index.html', { title: 'MkStone' });
+// });
+
+router.get('/tyj', function(req, res, next) {
+    res.render('/tyj/1.html', { title: 'MkStone' });
 });
 
+router.post('/qrcode', function(req, res, next) {
+    res.send({ msg: 'success' })
+});
 
-router.post('/api/other/wx/tb/midpage/', function(req, res, next) {
-    var reqBody = req.body
-    var twp = reqBody.twp
-    var ip = '193.112.75.80'
-    console.log('请求体:', reqBody)
-    console.log('淘口令', twp)
-
-    var api_url = `http://api.taokouling.com/tkl/tkljm?apikey=yyEARRKhpk&tkl=${twp}`;
-    fetch(api_url, {})
+router.post('/phone', function(req, res, next) {
+    var options = {
+        phone: req.body.phone
+    }
+    var api_url = 'http://localhost:3008/api/other/tz/phone';
+    fetch(api_url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic TWtTdG9uZV9iaTpEb3RhMl9iaQ=='
+            },
+            body: JSON.stringify(options)
+        })
         .then((response) => {
             if (response.ok) {
                 response.json().then(data => {
-                    console.log(data);
-                    var picUrl = data.picUrl || data.msg;
-                    var route = Date.now().toString();
-                    db.add(reqBody.twp, route, reqBody.alipay, reqBody.more, reqBody.xiaoweiba, picUrl, function() {
-                        res.send({
-                            route: `http://${ip}:3000/twp/${route}`,
-                            picUrl: picUrl,
-                        });
-                    })
+                    res.send({ msg: 'success' })
                 })
             }
             // 可以透過 blob(), json(), text() 轉成可用的資訊
@@ -39,21 +42,56 @@ router.post('/api/other/wx/tb/midpage/', function(req, res, next) {
         });
 });
 
+// router.post('/text', function(req, res, next) {
+//     console.log('123')
+//     res.send({ msg: 'success' })
+// });
+
+// router.post('/api/other/wx/tb/midpage/', function(req, res, next) {
+//     var reqBody = req.body
+//     var twp = reqBody.twp
+//     var ip = '193.112.75.80'
+//     console.log('请求体:', reqBody)
+//     console.log('淘口令', twp)
+
+//     var api_url = `http://api.taokouling.com/tkl/tkljm?apikey=yyEARRKhpk&tkl=${twp}`;
+//     fetch(api_url, {})
+//         .then((response) => {
+//             if (response.ok) {
+//                 response.json().then(data => {
+//                     console.log(data);
+//                     var picUrl = data.picUrl || data.msg;
+//                     var route = Date.now().toString();
+//                     db.add(reqBody.twp, route, reqBody.alipay, reqBody.more, reqBody.xiaoweiba, picUrl, function() {
+//                         res.send({
+//                             route: `http://${ip}:3000/twp/${route}`,
+//                             picUrl: picUrl,
+//                         });
+//                     })
+//                 })
+//             }
+//             // 可以透過 blob(), json(), text() 轉成可用的資訊
+
+//         }).catch((err) => {
+//             console.log('錯誤:', err);
+//         });
+// });
 
 
-router.get('/twp/*', function(req, res, next) {
-    var route = req.url.replace(/\/twp\//g, '')
-    db.findNoteByRoute(route, function(err, row) {
-        var picUrl = row.picUrl
-        res.render('taokouling', {
-            koulingPre: '复制框内整段文字，打开「手机掏寶ApP」即可「领取秘密券」并购买',
-            kouling: row.twp,
-            alipay: row.alipay,
-            wangzhan: row.more,
-            xiaoweiba: row.xiaoweiba,
-            picUrl: row.picUrl,
-        })
-    })
-});
+
+// router.get('/twp/*', function(req, res, next) {
+//     var route = req.url.replace(/\/twp\//g, '')
+//     db.findNoteByRoute(route, function(err, row) {
+//         var picUrl = row.picUrl
+//         res.render('taokouling', {
+//             koulingPre: '复制框内整段文字，打开「手机掏寶ApP」即可「领取秘密券」并购买',
+//             kouling: row.twp,
+//             alipay: row.alipay,
+//             wangzhan: row.more,
+//             xiaoweiba: row.xiaoweiba,
+//             picUrl: row.picUrl,
+//         })
+//     })
+// });
 
 module.exports = router;
